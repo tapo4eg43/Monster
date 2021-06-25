25bd8af3127f362ef9241b8d2e4e7d5bdb2fc1c5
tapo4eg43 <fpro@ukr.net>
2021-06-25 17:40:30 +0300

    .

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import './card.css';
 
 export const Card = (props) => (
-    <div>
+    <div className='card-container'>
+        <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
         <h1>{props.monster.name}</h1>
     </div>
 )
